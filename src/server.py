213efa2148032182ce04b68483a1c2be6340dcdf
smogork <krzysztof.smogor@wp.pl>
2022-12-06 21:02:03 +0100

    Reverse hello world text

--- src/server.py
+++ src/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World2!</p>"
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
